dca1644b965597c286aa79750342ce9e965dfa2a
Anton Meshchaninov <tonym7@yandex.ru>
2022-07-17 13:26:59 +0300

    finished cart

--- src/components/CatalogItem/CatalogItem.jsx
+++ src/components/CatalogItem/CatalogItem.jsx
@@ -2,23 +2,41 @@ import React from 'react'
 
 import styles from './CatalogItem.module.scss'
 
-const CatalogItem = ({ imgUrl, title, price, oldPrice }) => {
-	let discount = ''
+const CatalogItem = ({
+	imgUrl,
+	title,
+	price,
+	oldPrice,
+	quantity,
+}) => {
+	let infoBox = ''
+	const isSold = !quantity
+
+	isSold && (infoBox = 'Sold out')
+
 	oldPrice > price &&
-		(discount += `- ${Math.round((1 - price / oldPrice) * 100)}%`)
+		!isSold &&
+		(infoBox = `- ${Math.round((1 - price / oldPrice) * 100)}%`)
 
 	return (
 		<div className={styles.wrapper}>
-			<img className={styles.img} src={imgUrl} alt='Сережки' />
+			<div
+				className={`
+				${styles.img}
+				${isSold ? styles.sold : ''}
+			`}
+			>
+				<img src={imgUrl} alt='Сережки' />
+			</div>
 			<h3 className={styles.title}>{title}</h3>
 			<h4 className={styles.price}>{`$ ${price},00`}</h4>
 			<div
 				className={`
 				${styles.discount}
-				${discount ? styles.active : ''}
+				${infoBox ? styles.active : ''}
 			`}
 			>
-				{discount}
+				{infoBox}
 			</div>
 		</div>
 	)
